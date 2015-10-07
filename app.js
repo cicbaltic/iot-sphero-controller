704cc59a20c560f5ff38509b595639ebeb2731ae
Gytis Raciukaitis <gytis.raciukaitis@lt.ibm.com>
2015-10-07 22:00:08 +0300

    Add crude error handling.

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ var rspiState = {
     }
 };
 
+// always capture, log and exit on uncaught exceptions
+// your production system should auto-restart the app
+// this is the Node.js way
+process.on('uncaughtException', function(err) {
+  console.error('uncaughtException:', err.message);
+  console.error(err.stack);
+  //process.exit(1);
+});
 
 var config = {
   "org" :         "wjhtfb",
@@ -32,33 +40,42 @@ var spheroControls = new SpheroControls();
 
 var macOrb = {};
 
-deviceClient.on("connect", function(err, data) {
-    rspiState.rasp = "online";
-    console.log(rspiState);
-});
+function changeSpheroState(mac, state) {
+  if (mac) {
+    rspiState.spheros[mac] = state;
+  }
 
-//roConnect.connectAllSpheros();
+  deviceClient.publish("status", "json", JSON.stringify(rspiState));
+}
+
+deviceClient.on("connect", function(err, data) {
+  rspiState.rasp = "online";
+  console.log(rspiState);
+});
 
 spheroConnect.on("sphero_connected", function(mac, orb) {
-    console.log("Sphero %s connected.", mac);
-    macOrb[mac] = orb;
-    rspiState.spheros[mac] = "connected";
-    spheroControls.startCollisionDetection(macOrb[mac], mac);
+  console.log("Sphero %s connected.", mac);
+
+  macOrb[mac] = orb;
+  changeSpheroState(mac, "connected");
+
+  spheroControls.startCollisionDetection(macOrb[mac], mac);
 });
+
 spheroConnect.on("sphero_disconnected", function(mac, orb) {
-    console.log("Sphero %s disconnected.", mac);
-    delete macOrb[mac];
-    if (rspiState.spheros[mac]) {
-        rspiState.spheros[mac] = "disconnected";
-    }
+  console.log("Sphero %s disconnected.", mac);
+  delete macOrb[mac];
+  changeSpheroState(mac, "connected");
 });
+
 spheroControls.on("rolled", function(orb) {
-    var mac = spheroConnect.getMac(orb.connection.conn);
-    rspiState.spheros[mac] = "connected";
+  var mac = spheroConnect.getMac(orb.connection.conn);
+  changeSpheroState(mac, "connected");
 });
+
 spheroControls.on("collision", function(data, mac) {
-    console.log("collision detected on Sphero " + mac);
-    console.log(JSON.stringify(data));
+  console.log("collision detected on Sphero " + mac);
+  console.log(JSON.stringify(data));
 });
 
 deviceClient.on("command", function (commandName, format, payload, topic) {
@@ -68,36 +85,35 @@ deviceClient.on("command", function (commandName, format, payload, topic) {
     console.log("\tpayload: " + payload);
     console.log("\ttopic: " + topic + "\nend command.\n");
     if (commandName == "roll") {
-        var parameters = JSON.parse(payload)["params"];
+        var parameters = JSON.parse(payload).params;
         try {
-            spheroControls.rollForTime(macOrb[parameters["mac"]], parameters["speed"], parameters["direction"], parameters["time"]);
-            rspiState.spheros[parameters["mac"]] = "rolling";
-            deviceClient.publish("spheroStatus", "json", JSON.stringify(rspiState));
+            spheroControls.rollForTime(macOrb[parameters.mac], parameters.speed, parameters.direction, parameters.time);
+            rspiState.spheros[parameters.mac] = "rolling";
         } catch (e) {
             console.error("ERROR: your throw sucks");
             console.error(e);
         }
     } else if (commandName == "calibrate") {
-        var parameters = JSON.parse(payload)["params"];
-        spheroControls.calibrateEnd(macOrb[parameters["mac"]]);
-        rspiState.spheros[parameters["mac"]] = "calibrated";
+        var parameters = JSON.parse(payload).params;
+        spheroControls.calibrateEnd(macOrb[parameters.mac]);
+        rspiState.spheros[parameters.mac] = "calibrated";
     } else if (commandName == "getActiveSpheros") {
         try {
             deviceClient.publish("activeSpheros", "json", JSON.stringify(Object.keys(macOrb)));
         } catch (e) {
             console.error("ERROR: Got error message while tryin to list active spheros");
             console.error(e);
-        };
+        }
     } else if (commandName == "pairToSphero" ) {
-        var parameters = JSON.parse(payload)["params"];
+        var parameters = JSON.parse(payload).params;
         console.log(parameters);
-        var mac = parameters["mac"];
+        var mac = parameters.mac;
         macOrb[mac] = spheroConnect.connectSpheroOnMac(mac);
         if (macOrb[mac]) {
             macOrb[mac].on("ready", function(){
-                spheroControls.setColor(macOrb[mac], parameters["rgb"]);
+                spheroControls.setColor(macOrb[mac], parameters.rgb);
                 spheroControls.calibrateBegin(macOrb[mac]);
-                rspiState.spheros[parameters["mac"]] = "in_calibration";
+                rspiState.spheros[parameters.mac] = "in_calibration";
             });
         } else {
             var errorload = {};
